d6f20d103d133e61971e5188f0f4e1e802cec771
Robinson <robinson@dorkbox.com>
2022-01-07 14:55:58 +0100

    updated gradle utils version

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ import java.time.Instant
 gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
 
 plugins {
-    id("com.dorkbox.GradleUtils") version "2.14"
+    id("com.dorkbox.GradleUtils") version "2.16"
     id("com.dorkbox.Licensing") version "2.9.2"
     id("com.dorkbox.VersionUpdate") version "2.4"
     id("com.dorkbox.GradlePublish") version "1.11"
