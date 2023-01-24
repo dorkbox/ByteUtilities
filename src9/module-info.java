5fe84f169af7154f19182fc1d81b3d3cebf7d874
Robinson <robinson@dorkbox.com>
2023-01-24 13:31:27 +0100

    updated moduleinfo

--- src9/module-info.java
+++ src9/module-info.java
@@ -1,13 +1,11 @@
 module dorkbox.bytes {
     exports dorkbox.bytes;
 
-    requires dorkbox.updates;
+    requires transitive dorkbox.updates;
 
     requires static com.esotericsoftware.kryo;
     requires static io.netty.common;
     requires static io.netty.buffer;
 
-    requires kotlin.stdlib;
-
-    requires java.base;
+    requires transitive kotlin.stdlib;
 }
