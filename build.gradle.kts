165b8ce2c92790922b095ea7da17366195ba2993
Robinson <robinson@dorkbox.com>
2023-07-02 20:57:20 +0200

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ object Extras {
     // set for the project
     const val description = "Byte manipulation and SHA/xxHash utilities"
     const val group = "com.dorkbox"
-    const val version = "1.9"
+    const val version = "1.10"
 
     // set as project.ext
     const val name = "ByteUtilities"
